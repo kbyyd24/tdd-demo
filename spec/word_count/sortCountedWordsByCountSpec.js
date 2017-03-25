eef0868d230684075a52147f3a59d8a0dac67c8c
gaoyx <melo@gaoyuexiang.cn>
2017-03-26 00:05:45 +0800

    [refactor] use const to declare all varible in test cases

--- spec/word_count/sortCountedWordsByCountSpec.js
+++ spec/word_count/sortCountedWordsByCountSpec.js
@@ -1,16 +1,16 @@
 describe('test cases for sortCountedWord', () => {
   const sortCountedWordsByCount = require('../../lib/word_count/wordCount').sortCountedWordsByCount;
   it('should return empty array when given an empty array', function () {
-    let sortedCountedWords = sortCountedWordsByCount([]);
+    const sortedCountedWords = sortCountedWordsByCount([]);
     expect(sortedCountedWords).toEqual([]);
   });
   it('should return the same array when given an array with one countedWord', function () {
-    let countedWords = [{word: 'a', count: 3}];
-    let sortedCountedWords = sortCountedWordsByCount(countedWords);
+    const countedWords = [{word: 'a', count: 3}];
+    const sortedCountedWords = sortCountedWordsByCount(countedWords);
     expect(sortedCountedWords).toEqual(countedWords);
   });
   it('should return sortedCountedWord when given an array with countedWords', function () {
-    let countedWords = [
+    const countedWords = [
       {word: 'a', count: 3},
       {word: 'b', count: 5},
       {word: 'c', count: 9},
@@ -18,7 +18,7 @@ describe('test cases for sortCountedWord', () => {
       {word: 'e', count: 2},
       {word: 'f', count: 1}
     ];
-    let expectSortedCountedWords = [
+    const expectSortedCountedWords = [
       {word: 'c', count: 9},
       {word: 'b', count: 5},
       {word: 'a', count: 3},
@@ -26,7 +26,7 @@ describe('test cases for sortCountedWord', () => {
       {word: 'd', count: 1},
       {word: 'f', count: 1}
     ];
-    let sortedCountedWords = sortCountedWordsByCount(countedWords);
+    const sortedCountedWords = sortCountedWordsByCount(countedWords);
     expect(sortedCountedWords).toEqual(expectSortedCountedWords);
   });
 });
