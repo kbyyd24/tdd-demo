0f2cb96925ad8c37c4f532088c976413d3532ea2
gaoyx <melo@gaoyuexiang.cn>
2017-03-25 14:08:48 +0800

    delete all files

--- README.md
+++
@@ -1,4 +0,0 @@
-# tdd-demo
-
-This is a demo for the tdd excersise. 
-
--- spec/hello/HelloWorldSpec.js
+++
@@ -1,7 +0,0 @@
-describe("test suite: Hello world", function () {
-
-    it("test case 1: should return hello world", function () {
-        expect(helloWorld()).toEqual("Hello world!");
-    });
-
-});
