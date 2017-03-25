808e05bf32fca50a3148607626f2bacead09fa2a
gaoyx <melo@gaoyuexiang.cn>
2017-03-25 17:25:44 +0800

    [fixed #23] add test for given an array with countedWords

--- spec/word_count/wordCountSpec.js
+++ spec/word_count/wordCountSpec.js
@@ -87,4 +87,24 @@ describe('word count', function() {
     let sortedCountedWords = wordCount.sortCountedWordsByCount(countedWords)
     expect(sortedCountedWords).toEqual(countedWords)
   })
+  it('should return sortedCountedWord when given an array with countedWords', function () {
+    let countedWords = [
+      {word: 'a', count: 3},
+      {word: 'b', count: 5},
+      {word: 'c', count: 9},
+      {word: 'd', count: 1},
+      {word: 'e', count: 2},
+      {word: 'f', count: 1}
+    ]
+    let expectSortedCountedWords = [
+      {word: 'c', count: 9},
+      {word: 'b', count: 5},
+      {word: 'a', count: 3},
+      {word: 'e', count: 2},
+      {word: 'd', count: 1},
+      {word: 'f', count: 1}
+    ]
+    let sortedCountedWords = wordCount.sortCountedWordsByCount(countedWords)
+    expect(sortedCountedWords).toEqual(expectSortedCountedWords)
+  })
 })
