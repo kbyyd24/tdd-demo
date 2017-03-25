d1caf78560112b2030d905e30381d23fc7e6f92f
gaoyx <melo@gaoyuexiang>
2017-03-25 18:03:17 +0800

    [#27] add test for given a file with words

--- spec/word_count/wordCountSpec.js
+++ spec/word_count/wordCountSpec.js
@@ -107,6 +107,7 @@ describe('word count', function() {
     let sortedCountedWords = wordCount.sortCountedWordsByCount(countedWords)
     expect(sortedCountedWords).toEqual(expectSortedCountedWords)
   })
+
   it('should return empty array when given a file with no content', function () {
     wordCount.countAndSortWordsFromFile('emptyFile', (sortedCountedWords) => {
       expect(sortedCountedWords).toEqual([])
@@ -115,6 +116,19 @@ describe('word count', function() {
   it('should return an array with one sortedCountedWord when given a file with one word', function () {
     wordCount.countAndSortWordsFromFile('oneWordFile', (sortedCountedWord) => {
       expect(sortedCountedWord).toEqual([{word: 'a', count: 1}])
+    })
+  })
+  it('should return an array with sortedCountedWords when given a file with words', function () {
+    let expectSortedCountedWords = [
+      {word: 'c', count: 9},
+      {word: 'b', count: 5},
+      {word: 'a', count: 3},
+      {word: 'e', count: 2},
+      {word: 'd', count: 1},
+      {word: 'f', count: 1}
+    ]
+    wordCount.countAndSortWordsFromFile('wordsFile', (sortedCountedWords) => {
+      expect(sortedCountedWords).toEqual(expectSortedCountedWords)
     })
   })
 })
