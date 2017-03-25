0871afdb5a3084fe1ac6e5392d16509d47320598
gaoyx <melo@gaoyuexiang>
2017-03-25 23:55:08 +0800

    [refactor] move test cases for sortCountedWordsByCount to sortCountedWordsByCountSpec.js

---
+++ spec/word_count/sortCountedWordsByCountSpec.js
@@ -0,0 +1,32 @@
+describe('test cases for sortCountedWord', () => {
+  let sortCountedWordsByCount = require('../../lib/word_count/wordCount').sortCountedWordsByCount;
+  it('should return empty array when given an empty array', function () {
+    let sortedCountedWords = sortCountedWordsByCount([]);
+    expect(sortedCountedWords).toEqual([]);
+  });
+  it('should return the same array when given an array with one countedWord', function () {
+    let countedWords = [{word: 'a', count: 3}];
+    let sortedCountedWords = sortCountedWordsByCount(countedWords);
+    expect(sortedCountedWords).toEqual(countedWords);
+  });
+  it('should return sortedCountedWord when given an array with countedWords', function () {
+    let countedWords = [
+      {word: 'a', count: 3},
+      {word: 'b', count: 5},
+      {word: 'c', count: 9},
+      {word: 'd', count: 1},
+      {word: 'e', count: 2},
+      {word: 'f', count: 1}
+    ];
+    let expectSortedCountedWords = [
+      {word: 'c', count: 9},
+      {word: 'b', count: 5},
+      {word: 'a', count: 3},
+      {word: 'e', count: 2},
+      {word: 'd', count: 1},
+      {word: 'f', count: 1}
+    ];
+    let sortedCountedWords = sortCountedWordsByCount(countedWords);
+    expect(sortedCountedWords).toEqual(expectSortedCountedWords);
+  });
+});
--- spec/word_count/wordCountSpec.js
+++ spec/word_count/wordCountSpec.js
@@ -1,38 +1,6 @@
 describe('word count', function() {
   let wordCount = require('../../lib/word_count/wordCount.js');
 
-
-
-  it('should return empty array when given an empty array', function () {
-    let sortedCountedWords = wordCount.sortCountedWordsByCount([]);
-    expect(sortedCountedWords).toEqual([]);
-  });
-  it('should return the same array when given an array with one countedWord', function () {
-    let countedWords = [{word: 'a', count: 3}];
-    let sortedCountedWords = wordCount.sortCountedWordsByCount(countedWords);
-    expect(sortedCountedWords).toEqual(countedWords);
-  });
-  it('should return sortedCountedWord when given an array with countedWords', function () {
-    let countedWords = [
-      {word: 'a', count: 3},
-      {word: 'b', count: 5},
-      {word: 'c', count: 9},
-      {word: 'd', count: 1},
-      {word: 'e', count: 2},
-      {word: 'f', count: 1}
-    ];
-    let expectSortedCountedWords = [
-      {word: 'c', count: 9},
-      {word: 'b', count: 5},
-      {word: 'a', count: 3},
-      {word: 'e', count: 2},
-      {word: 'd', count: 1},
-      {word: 'f', count: 1}
-    ];
-    let sortedCountedWords = wordCount.sortCountedWordsByCount(countedWords);
-    expect(sortedCountedWords).toEqual(expectSortedCountedWords);
-  });
-
   it('should return empty array when given a file with no content', function () {
     wordCount.countAndSortWordsFromFile('emptyFile', (sortedCountedWords) => {
       expect(sortedCountedWords).toEqual([]);
