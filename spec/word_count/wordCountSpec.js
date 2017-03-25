0477b413ab6a229bb7afeac8deba76d59cfaecb4
gaoyx <melo@gaoyuexiang.cn>
2017-03-25 14:49:20 +0800

    [fixed #2] fixed test of should retrun empty array when given a file name which content is empty

--- spec/word_count/wordCountSpec.js
+++ spec/word_count/wordCountSpec.js
@@ -1,5 +1,5 @@
 describe('word count', function() {
-
+  let getFileContentAsArray = require('../../lib/word_count/wordCount.js')
   it('should return empty array when given a file name which content is empty', function() {
     let emptyFilName = 'emptyFile'
     let words = getFileContentAsArray()
