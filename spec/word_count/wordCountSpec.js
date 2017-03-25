d7de880713ec696fa5bd2a9ca1c499b335cbb421
gaoyx <melo@gaoyuexiang.cn>
2017-03-25 23:50:42 +0800

    [refacotr] move test cases for countWord to countWordSpec.js

--- spec/word_count/wordCountSpec.js
+++ spec/word_count/wordCountSpec.js
@@ -1,37 +1,7 @@
 describe('word count', function() {
   let wordCount = require('../../lib/word_count/wordCount.js');
 
-  it('should return empty array when given an empty array for countWord', function () {
-    let countedWords = wordCount.countWord([]);
-    expect(countedWords).toEqual([]);
-  });
-  it('should return countedWords and each count is 1 when given an array with no repeated word', function () {
-    let words = ['a', 'b', 'c', 'd', 'e', 'f'];
-    let countedWords = wordCount.countWord(words);
-    for (let countedWord of countedWords) {
-      expect(countedWord.count).toBe(1);
-    }
-  });
-  it('should return countedWords when given an array with repeated words', function () {
-    let words = [
-      'a', 'a', 'a',
-      'b', 'b', 'b', 'b', 'b',
-      'c', 'c', 'c', 'c', 'c', 'c', 'c', 'c', 'c',
-      'd',
-      'e', 'e',
-      'f'
-    ];
-    let expectCountedWords = [
-      {word: 'a', count: 3},
-      {word: 'b', count: 5},
-      {word: 'c', count: 9},
-      {word: 'd', count: 1},
-      {word: 'e', count: 2},
-      {word: 'f', count: 1}
-    ];
-    let countedWords = wordCount.countWord(words);
-    expect(countedWords).toEqual(expectCountedWords);
-  });
+
 
   it('should return empty array when given an empty array', function () {
     let sortedCountedWords = wordCount.sortCountedWordsByCount([]);
